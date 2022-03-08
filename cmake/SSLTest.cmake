15b16fa33f88b62da59db957c5d7ae90e8b593fc
Noah Pendleton <2538614+noahp@users.noreply.github.com>
2022-03-08 10:15:58 -0500

    MFLT-5638 cmake recipe for expat version conflict (#6)
    The conda-forge cmake package has cmake requirement of:
    
    `expat >=2.4.1,<3.0a0`
    
    This conflicts with our internally built gdb packages, which are
    (incorrectly) set to ^2.2:
    
    `expat >=2.2.10,<2.3.0a0`
    
    It's simpler to just fork cmake especially since we only use it in
    testing, not in production.
    
    Recipe taken from here:
    
    https://github.com/conda-forge/cmake-feedstock/tree/284e91676110b5d52130bd6fb04e91f099194fcf/recipe
    
    Only modification is to specify `expat: '2.2'` instead of `expat: '2'`.

---
+++ cmake/SSLTest.cmake
@@ -0,0 +1,16 @@
+set(FILE_NAME "LICENSE.txt")
+set(DOWNLOAD_URL "https://raw.githubusercontent.com/conda-forge/cmake-feedstock/968a712d28a374c73e20b8b8b0115e8027018870/${FILE_NAME}")
+set(EXPECTED_SHA256 "b5904c52eaee178d332cc0cb2e3795f68af62a72bfb090ea32c493abd88af0d6")
+
+file(DOWNLOAD ${DOWNLOAD_URL} ${CMAKE_CURRENT_BINARY_DIR}/${FILE_NAME}
+ SHOW_PROGRESS
+ EXPECTED_HASH  SHA256=${EXPECTED_SHA256}
+ STATUS STATUS
+ TLS_VERIFY on )
+
+list( GET STATUS 0 RET )
+list( GET STATUS 1 MESSAGE )
+
+if( NOT RET EQUAL 0 )
+  message(FATAL "Error Downloading file: ${MESSAGE}")
+endif()
