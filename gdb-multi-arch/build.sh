ba3c937c6f65b0b321088dfd8e44974b8c242f4b
Tyler Hoffman <tyler@memfault.com>
2022-03-15 18:22:01 -0700

    Update GDB to 11.2

--- gdb-multi-arch/build.sh
+++ gdb-multi-arch/build.sh
@@ -9,7 +9,8 @@ export REAL_TARGET=multi-arch
 export FAKE_TARGET_PREFIX="${PREFIX}/${FAKE_TARGET}"
 export REAL_TARGET_PREFIX="${PREFIX}/${REAL_TARGET}"
 
-export CPPFLAGS="$CPPFLAGS -I$PREFIX/include"
+export CPPFLAGS="$CPPFLAGS -fcommon -I$PREFIX/include -Wno-constant-logical-operand"
+export CFLAGS="$CFLAGS -Wno-constant-logical-operand -Wno-format-nonliteral -Wno-self-assign"
 # Setting /usr/lib/debug as debug dir makes it possible to debug the system's
 # python on most Linux distributions
 ./configure \
