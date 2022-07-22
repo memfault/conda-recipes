3d02e8f745529d52da63b0545ba5ab47d18499d0
Tyler Hoffman <tyler@memfault.com>
2022-07-22 14:24:47 -0700

    Crash at the right time

--- gdb-multi-arch/build.sh
+++ gdb-multi-arch/build.sh
@@ -9,8 +9,9 @@ export REAL_TARGET=multi-arch
 export FAKE_TARGET_PREFIX="${PREFIX}/${FAKE_TARGET}"
 export REAL_TARGET_PREFIX="${PREFIX}/${REAL_TARGET}"
 
-export CPPFLAGS="$CPPFLAGS -fcommon -I$PREFIX/include -Wno-constant-logical-operand"
-export CFLAGS="$CFLAGS -Wno-constant-logical-operand -Wno-format-nonliteral -Wno-self-assign"
+export CPPFLAGS="$CPPFLAGS -fcommon -I$PREFIX/include -Wno-constant-logical-operand -ggdb"
+export CFLAGS="$CFLAGS -Wno-constant-logical-operand -Wno-format-nonliteral -Wno-self-assign -ggdb"
+export CXXFLAGS="$CXXFLAGS -ggdb"
 # Setting /usr/lib/debug as debug dir makes it possible to debug the system's
 # python on most Linux distributions
 ./configure \
@@ -35,32 +36,10 @@ export CFLAGS="$CFLAGS -Wno-constant-logical-operand -Wno-format-nonliteral -Wno
 make
 make install
 
-# Move from the fake to real directory
-mv "${FAKE_TARGET_PREFIX}" "${REAL_TARGET_PREFIX}"
+# At this point the gdb file will be located at 
+#   ../work/gdb/gdb
+# So you can debug it with:
+#   $ cd $HOME/mambaforge/envs/build/conda-bld/multi-arch-gdb_1658520479521/work/gdb/
+#   $ /usr/local/bin/gdb --args ./gdb $HOME/dev/memfault/memfault/tools/tests/fixtures/binaries/symbols/heap-analyzer-example.elf
+exit 1
 
-# Enable matching with ! (not) bash operator
-shopt -s extglob
-
-# Delete every binary except addr2line, gdb, gdb-add-index, objcopy, objdump, and size.
-pushd "${REAL_TARGET_PREFIX}"/bin/
-    rm !(arm-elf-linux-addr2line|arm-elf-linux-gdb|arm-elf-linux-gdb-add-index|arm-elf-linux-objdump|arm-elf-linux-objcopy|arm-elf-linux-size|arm-elf-linux-strings)
-    mv arm-elf-linux-addr2line addr2line
-    mv arm-elf-linux-gdb gdb
-    mv arm-elf-linux-gdb-add-index gdb-add-index
-    mv arm-elf-linux-objcopy objcopy
-    mv arm-elf-linux-objdump objdump
-    mv arm-elf-linux-size size
-    mv arm-elf-linux-strings strings
-    strip addr2line
-    strip gdb
-    strip objdump
-    strip objcopy
-    strip size
-    strip strings
-popd
-
-
-# Symlink every binary from the build into /bin
-pushd "${PREFIX}"/bin
-    ln -s "${REAL_TARGET_PREFIX}"/bin/* ./
-popd
