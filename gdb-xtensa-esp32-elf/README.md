03c66da580cad8d2644bdc68029ee96dece20a94
Noah Pendleton <2538614+noahp@users.noreply.github.com>
2022-07-26 11:53:52 -0400

    ESP32 GDB binary recipe

--- gdb-xtensa-esp32-elf/README.md
+++ gdb-xtensa-esp32-elf/README.md
@@ -1,1 +1,16 @@
-This builds the `xtensa-esp32-elf-gdb` binaries and binutil extras.
+# gdb-xtensa-esp32-elf
+
+This builds the `xtensa-esp32-elf-gdb` package.
+
+Originally this built the packages from source to enable specific python
+versions (the pre-compiled versions from espressif were python2 only).
+
+As of
+https://github.com/espressif/binutils-gdb/releases/tag/esp-gdb-v11.1_20220318 ,
+espressif ships pre-compiled versions for all current python3 versions,
+including python3.8 (and nominally python3.10).
+
+So this package switched to using the pre-compiled release, instead of building
+from source. Down the road it might be worth investigating a build from source,
+but that might require reaching out to espressif to see what build scripts they
+use to generate these binaries.
--- gdb-xtensa-esp32-elf/build.sh
+++ gdb-xtensa-esp32-elf/build.sh
@@ -1,42 +1,19 @@
 #!/bin/bash
 
-# Copy the ESP32 "overlay" files:
-cp -av $SRC_DIR/crosstool-NG-esp32/overlays/xtensa_esp32/binutils/. $SRC_DIR/binutils-gdb-esp32-src/
-cp -av $SRC_DIR/crosstool-NG-esp32/overlays/xtensa_esp32/gdb/. $SRC_DIR/binutils-gdb-esp32-src/
+set -ex
 
-export TARGET=xtensa-esp32-elf
-export TARGET_PREFIX="${PREFIX}/${TARGET}"
+TARGET=xtensa-esp32-elf
 
-export CPPFLAGS="$CPPFLAGS -I$PREFIX/include"
-# Setting /usr/lib/debug as debug dir makes it possible to debug the system's
-# python on most Linux distributions
+# Copy everything into a TARGET-namespaced directory. This is so we don't
+# overwrite other GDB versions' bin/lib/include/share etc. files, which might
+# not be target-prefixed!
+mkdir -p "${PREFIX}/${TARGET}"
+cp -r ${SRC_DIR}/* "${PREFIX}/${TARGET}"
 
-cd binutils-gdb-esp32-src
-
-./configure \
-    --prefix="${TARGET_PREFIX}" \
-    --target=$TARGET \
-    --with-separate-debug-dir="${TARGET_PREFIX}/lib/debug:/usr/lib/debug" \
-    --with-lzma \
-    --with-expat \
-    --with-libexpat-prefix="$PREFIX" \
-    --with-libiconv-prefix="$PREFIX" \
-    --without-libunwind-ia64 \
-    --with-zlib \
-    --without-babeltrace \
-    --with-python="$PREFIX" \
-    --disable-threads \
-    --disable-sim \
-    --disable-nls \
-    --disable-binutils \
-    --disable-ld \
-    --disable-gas \
-    --disable-sim \
-    --disable-gold
-make
-make install
-
-# Symlink every binary from the build into /bin
-pushd "${PREFIX}"/bin
+# Symlink the binaries into the PREFIX/bin directory so they're available on
+# PATH
+(
+    # cd so we can use relative paths + wildcard for the symlink
+    cd "${PREFIX}/bin"
     ln -s ../"${TARGET}"/bin/* ./
-popd
+)
