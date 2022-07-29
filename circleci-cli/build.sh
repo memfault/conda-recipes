8f3f481460173c1d9d1155cd7db60c2be53a43f2
Noah Pendleton <2538614+noahp@users.noreply.github.com>
2022-07-29 12:33:51 -0400

    Circleci-cli recipe for osx-arm64 (#25)
    Update the source version, there's some weird embedded `_data/data.yml`
    file that doesn't make it into the binary with the default build rule on
    the old version.
    
    No idea what upstream did on the prebuilt releases 🤷

--- circleci-cli/build.sh
+++ circleci-cli/build.sh
@@ -1,5 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-mkdir $PREFIX/bin
-cp $SRC_DIR/circleci $PREFIX/bin
-chmod +x $PREFIX/bin/circleci
+make
+
+# the output binary is located at ex: 'build/linux/amd64/circleci'
+mkdir -p $PREFIX/bin
+cp $SRC_DIR/build/*/*/circleci $PREFIX/bin
