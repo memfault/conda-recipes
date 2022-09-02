674baf43d27b490deefe781cc3297764b8c232d9
Noah Pendleton <2538614+noahp@users.noreply.github.com>
2022-09-02 15:22:19 -0400

    Build esp32 gdb multibin from source
    Update the recipe to build with the `--with-xtensaconfig` custom
    configuration and library that enables debugging esp32+esp32s2+esp32s3
    with a single gdb binary. This was made possible thanks to Espressif
    open sourcing the necessary `xtensaconfig` submodule repo:
    
    https://github.com/espressif/esp-idf/issues/9459
    
    Version updated to `memfault1` from `memfault0` to distinguish this
    flavor from the previous version.
    
    I've tested on linux-x86_64 and mac-x86_64.

--- gdb-xtensa-esp32-elf/build.sh
+++ gdb-xtensa-esp32-elf/build.sh
@@ -1,18 +1,53 @@
-#!/bin/bash
+#!/bin/usr/env bash
 
-# Copy the ESP32 "overlay" files:
-cp -av $SRC_DIR/crosstool-NG-esp32/overlays/xtensa_esp32/binutils/. $SRC_DIR/binutils-gdb-esp32-src/
-cp -av $SRC_DIR/crosstool-NG-esp32/overlays/xtensa_esp32/gdb/. $SRC_DIR/binutils-gdb-esp32-src/
+###
+# Note
+#
+# This script is partially based on:
+# https://github.com/espressif/binutils-gdb/blob/esp-gdb-v11.1_20220318/build_esp_gdb.sh
+###
 
-export TARGET=xtensa-esp32-elf
-export TARGET_PREFIX="${PREFIX}/${TARGET}"
+# The tool name should be 'xtensa-esp-elf', the same as how Espressif names it
+export TARGET=xtensa-esp-elf
+# Installation directory for the toolchain is 'xtensa-esp32-elf' to distinguish
+# from esp8266 etc.
+export TARGET_DIR=xtensa-esp32-elf
+export TARGET_PREFIX="${PREFIX}/${TARGET_DIR}"
 
 export CPPFLAGS="$CPPFLAGS -I$PREFIX/include"
 # Setting /usr/lib/debug as debug dir makes it possible to debug the system's
 # python on most Linux distributions
 
-cd binutils-gdb-esp32-src
+export ESP_CHIP_ARCHITECTURE=xtensa
+export GDB_DIST="$PWD"/dist
 
+# Disable PLATFORM selection for the xtensaconfig build. This disables the bin-wrapper script from being
+# included in the xtensaconfig output. We don't really want it- it's used to at
+# runtime check if python is available, but since this is a conda package, in a
+# healthy compatible environment, it will be.
+PLATFORM=
+
+# Clear the PREFIX variable temporarily so the xtensa-config lib install works
+# correctly:
+# https://github.com/espressif/esp-xtensaconfig-lib/blob/5db415200f750f871b40b2ef5860d3d01275ae8b/Makefile#L83-L84
+_PREFIX=${PREFIX}
+unset PREFIX
+
+# Build xtensa-config libs
+mkdir -p "${GDB_DIST}"/lib
+pushd xtensaconfig
+make clean
+# AR="$TARGET_HOST-ar" CC="$TARGET_HOST-gcc"
+TARGET_ESP_ARCH=${ESP_CHIP_ARCHITECTURE} DESTDIR="${GDB_DIST}" PLATFORM=$PLATFORM make install
+popd
+# Install xtensa-config libs
+mkdir -p "$TARGET_PREFIX"/lib
+cp -R "${GDB_DIST}"/lib "$TARGET_PREFIX"/
+
+# Restore PREFIX variable
+PREFIX=${_PREFIX}
+
+# Configure, build, and install gdb
 ./configure \
     --prefix="${TARGET_PREFIX}" \
     --target=$TARGET \
@@ -21,6 +56,9 @@ cd binutils-gdb-esp32-src
     --with-expat \
     --with-libexpat-prefix="$PREFIX" \
     --with-libiconv-prefix="$PREFIX" \
+    --with-libexpat-type=static \
+    --with-libgmp-type=static \
+    --with-static-standard-libraries \
     --without-libunwind-ia64 \
     --with-zlib \
     --without-babeltrace \
@@ -31,12 +69,26 @@ cd binutils-gdb-esp32-src
     --disable-binutils \
     --disable-ld \
     --disable-gas \
+    --without-guile \
     --disable-sim \
-    --disable-gold
-make
+    --disable-gold \
+    --with-pkgversion="esp-gdb" \
+    --with-xtensaconfig
+make -j${CPU_COUNT}
 make install
+
+mkdir -p "$TARGET_PREFIX"/bin/
+
+# Wrapper scripts for esp32* variants
+cp "$SRC_DIR"/xtensa-esp32*-elf-gdb "$TARGET_PREFIX"/bin/
+chmod +x "$TARGET_PREFIX"/bin/*
 
 # Symlink every binary from the build into /bin
 pushd "${PREFIX}"/bin
-    ln -s ../"${TARGET}"/bin/* ./
+    ln -s ../"${TARGET_DIR}"/bin/* ./
+popd
+
+# Same for lib
+pushd "${PREFIX}"/lib
+    ln -s ../"${TARGET_DIR}"/lib/* ./
 popd
