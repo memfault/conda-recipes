b88f54f2cd7d7ae439799ac95d74dfbe90ff1491
Noah Pendleton <2538614+noahp@users.noreply.github.com>
2022-09-12 08:52:49 -0400

    Update memfault-config to disable python keyring (#32)
    Workaround for a poetry issue.

--- memfault-config/activate.sh
+++ memfault-config/activate.sh
@@ -1,1 +1,5 @@
 export MEMFAULT_DEPS_ROOT="$CONDA_PREFIX"
+
+# This works around a python poetry issue:
+# https://github.com/python-poetry/poetry/issues/1917
+export PYTHON_KEYRING_BACKEND=keyring.backends.null.Keyring
