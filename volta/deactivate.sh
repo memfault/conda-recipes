88cec44e01351db609e0ae9ff32348abf66560a4
Noah Pendleton <2538614+noahp@users.noreply.github.com>
2023-09-26 10:48:57 -0400

    Prefer existing VOLTA_HOME if already set
    This enables using an arm64 volta install (i.e. no rosetta for node)
    with an x86 conda environment on macos-arm64.

--- volta/deactivate.sh
+++ volta/deactivate.sh
@@ -1,6 +1,15 @@
-# if VOLTA_HOME_BACKUP is set, restore it to VOLTA_HOME
-unset VOLTA_HOME
-if [ -n "$VOLTA_HOME_BACKUP" ]; then
-  export VOLTA_HOME="$VOLTA_HOME_BACKUP"
-  unset VOLTA_HOME_BACKUP
+# if VOLTA_HOME_PREFERRED is set, don't unset VOLTA_HOME; the user activated the
+# conda environment with VOLTA_HOME already set, and we don't want to mess with
+# it
+if [ -n "$VOLTA_HOME_PREFERRED" ]; then
+  unset VOLTA_HOME_PREFERRED
+else
+  # otherwise, clean up after ourselves
+  directory_to_remove=$VOLTA_HOME/bin
+  # from https://unix.stackexchange.com/a/108933
+  PATH=:$PATH:
+  PATH=${PATH//:$directory_to_remove:/:}
+  PATH=${PATH#:}
+  PATH=${PATH%:}
+  unset VOLTA_HOME
 fi
