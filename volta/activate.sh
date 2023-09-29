59e3042d1e91758a75c1a8451a478961ac24b431
Martijn Thé <martijnthe@users.noreply.github.com>
2023-09-29 11:59:20 +0200

    fix(volta): allow activate.sh to work in strict mode
      ### Summary
    
    If the script is sourced when run in strict mode (`-u`) (disallowing unbound variables, the activate.sh script would fail because `VOLTA_HOME` was unbound. This PR adds a fallback to avoid this error.
    
      ## Test plan
    
    Add `set -u` at the top of the script and run it. Observe it no longer fails.

--- volta/activate.sh
+++ volta/activate.sh
@@ -1,6 +1,6 @@
 # if VOLTA_HOME is set, don't override it- save it so the deactivate script
 # knows to not unset it
-if [ -n "$VOLTA_HOME" ]; then
+if [ -n "${VOLTA_HOME-}" ]; then
   export VOLTA_HOME_PREFERRED="$VOLTA_HOME"
 else
   # set the volta directory to reside inside the conda environment
