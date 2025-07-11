5a3664e2a4db1856d38eb4f2e54d5da0a578b49b
Avery Fischer <avery@memfault.com>
2025-07-11 13:47:51 +0200

    tests

--- gdb-xtensa-lx106-elf/build.sh
+++ gdb-xtensa-lx106-elf/build.sh
@@ -1,58 +1,9 @@
-#!/bin/bash
+#!/ust/bin/env bash
 
-# Copy the ESP8266 "overlay" files:
-cp -av $SRC_DIR/xtensa-overlays/xtensa_lx106/binutils/. $SRC_DIR/binutils-gdb-lx106-src/
-cp -av $SRC_DIR/xtensa-overlays/xtensa_lx106/gdb/. $SRC_DIR/binutils-gdb-lx106-src/
+mkdir -p "${PREFIX}/bin"
 
-export TARGET=xtensa-lx106-elf
-export TARGET_PREFIX="${PREFIX}/${TARGET}"
+cp -R "${SRC_DIR}"/bin/xtensa-lx106*-elf-gdb "${PREFIX}"/bin
+cp -R "${SRC_DIR}/bin/xtensa-esp-elf-gdb-${PY_VER}" "${PREFIX}"/bin
+cp -R "${SRC_DIR}"/lib "${PREFIX}"
+cp -R "${SRC_DIR}"/share "${PREFIX}"
 
-export CPPFLAGS="$CPPFLAGS -I$PREFIX/include"
-# Setting /usr/lib/debug as debug dir makes it possible to debug the system's
-# python on most Linux distributions
-
-cd binutils-gdb-lx106-src
-
-if [ `uname` == Darwin ]; then
-  EXTRA_CONFIGURE_FLAGS=""
-else
-  EXTRA_CONFIGURE_FLAGS="--with-debuginfod"
-fi
-
-./configure \
-    --prefix="${TARGET_PREFIX}" \
-    --target=$TARGET \
-    --with-separate-debug-dir="${TARGET_PREFIX}/lib/debug:/usr/lib/debug" \
-    --with-lzma \
-    --with-expat \
-    --with-libexpat-prefix="$PREFIX" \
-    --with-libiconv-prefix="$PREFIX" \
-    --without-libunwind-ia64 \
-    --with-zlib \
-    --without-babeltrace \
-    --with-python="$PREFIX" \
-    $EXTRA_CONFIGURE_FLAGS \
-    --disable-threads \
-    --disable-sim \
-    --disable-nls \
-    --disable-binutils \
-    --disable-ld \
-    --disable-gas \
-    --disable-sim \
-    --disable-gold
-make
-make install
-
-shopt -s extglob
-
-# Delete every binary except gdb
-pushd "${TARGET_PREFIX}"/bin/
-    rm !(xtensa-lx106-elf-gdb)
-    strip xtensa-lx106-elf-gdb
-popd
-
-
-# Symlink every binary from the build into /bin
-pushd "${PREFIX}"/bin
-    ln -s "${TARGET_PREFIX}"/bin/* ./
-popd
