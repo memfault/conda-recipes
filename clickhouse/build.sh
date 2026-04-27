47a2aea93ee3e87a1ef53dd066db36933ce84308
Marc Jessome <marc@memfault.com>
2026-04-27 14:19:39 -0400

    linux-aarch64 for clickhouse

--- clickhouse/build.sh
+++ clickhouse/build.sh
@@ -10,10 +10,16 @@ ARCH=$(uname -m)
 if [ "${OS}" = "Linux" ]; then
   cp $SRC_DIR/usr/bin/clickhouse $TARGET_BIN
 
+  if [ "${ARCH}" = "x86_64" -o "${ARCH}" = "amd64" ]; then
+    UPX_ARCH="amd64"
+  elif [ "${ARCH}" = "aarch64" -o "${ARCH}" = "arm64" ]; then
+    UPX_ARCH="arm64"
+  fi
+
   # the clickhouse binary is like 500MB on linux. let's pack it.
-  wget https://github.com/upx/upx/releases/download/v5.1.1/upx-5.1.1-amd64_linux.tar.xz
-  tar -xf upx-5.1.1-amd64_linux.tar.xz
-  ./upx-5.1.1-amd64_linux/upx $TARGET_BIN
+  wget https://github.com/upx/upx/releases/download/v5.1.1/upx-5.1.1-${UPX_ARCH}_linux.tar.xz
+  tar -xf upx-5.1.1-${UPX_ARCH}_linux.tar.xz
+  ./upx-5.1.1-${UPX_ARCH}_linux/upx $TARGET_BIN
 elif [ "${OS}" = "Darwin" ]; then
   if [ "${ARCH}" = "x86_64" -o "${ARCH}" = "amd64" ]; then
     ARCH_SUFFIX=""
